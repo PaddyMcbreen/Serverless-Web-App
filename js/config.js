d3890b2f8c212176e68444b8a9f5537bdcb18efc
PaddyMcbreen <patrickmcbreen2004@hotmail.com>
2024-02-01 15:37:27 +0000

    S3 Bucket Pulled

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-2_eetYbtpwE', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'hlkrga5dse1lhjdv9pu41pkh4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://fhmuqmt9lh.execute-api.eu-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
